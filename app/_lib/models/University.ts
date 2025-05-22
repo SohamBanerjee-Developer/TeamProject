41449091f156850f665599a03293fcf5494b137d
Biswajit Das <workbiswajit@gmail.com>
2025-05-22 07:43:47 +0530

    Add university schema

---
+++ app/_lib/models/University.ts
@@ -0,0 +1,25 @@
+// models/University.ts
+import {Schema, Document, models, model, Model} from 'mongoose';
+
+export interface IUniversity extends Document {
+    name: string;
+    location: string;
+    coverImage: string;
+    description: string;
+    governmentId: string;
+}
+
+const UniversitySchema: Schema<IUniversity> = new Schema(
+    {
+        name: { type: String, required: true },
+        location: { type: String, required: true },
+        coverImage: { type: String, required: true },
+        description: { type: String, required: true },
+        governmentId: { type: String, required: true },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export const univerSity:Model<IUniversity> =   models.University || model<IUniversity>('University', UniversitySchema);
