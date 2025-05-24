39e33450302ef1ce8d56ca0ddc71fd28724ae1e7
Soham Banerjee <sohambanerjee384@gmail.com>
2025-05-24 16:50:18 +0530

    adding basic structure for post

---
+++ app/api/v1/Community/post/route.ts
@@ -0,0 +1,38 @@
+import { asyncHandler } from "@/app/_utils/helper";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = asyncHandler(
+    async (req: NextRequest)=>{
+    const body = await req.json()
+    const universityId = body.universityId
+     
+    
+    return NextResponse.json({
+        message: "got all the posts"
+    })
+   
+})
+
+
+
+export function POST (){
+
+    NextResponse.json({
+        message: "successfully posted"
+    })
+    
+}
+
+export function PUT(){
+
+    NextResponse.json({
+        message: " successfully updated the post"
+    })
+}
+
+export function DELETE () {
+
+    NextResponse.json({
+        message: " successfully deleted all the posts"
+    })
+}
