bb5bc3ac56d357976f7aa39b29679a37886e4b21
Soham Banerjee <sohambanerjee384@gmail.com>
2025-05-24 18:12:16 +0530

    added a new collection called post and renamed comment.ts

--- app/_lib/models/Conversation.ts
+++ app/_lib/models/Conversation.ts
@@ -8,6 +8,29 @@ export interface IComment extends Document {
 
 }
 
+interface post extends IComment{
+    universityId: Schema.Types.ObjectId
+}
+
+const postSchema = new Schema<post>({
+    universityId: {type: Schema.Types.ObjectId, ref: "university"},
+    userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
+
+    postId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        refPath: "postModel"
+    },
+    postModel: {
+        type: String,
+        required: true,
+        enum: ["PGPost", "Store", "POST", "Comment"]
+    },
+
+    comment: { type: String, required: true },
+
+}, {timestamps: true});
+
 const commentSchema = new Schema<IComment>({
     userId: { type: Schema.Types.ObjectId, ref: "User", required: true },
 
@@ -26,4 +49,5 @@ const commentSchema = new Schema<IComment>({
 
 }, {timestamps: true});
 
+export const Post:Model<post> = model<post>("Posts", postSchema )
 export const Comment:Model<IComment> = model<IComment>("Comment", commentSchema);
